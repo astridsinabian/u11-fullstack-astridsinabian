74e6964e8d1d834b3006b90d2621326be4960000
Astrid <astrid.sinabian@chasacademy.se>
2019-09-02 15:31:49 +0200

    Started with login

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
-const { registerValidation } = require('../validation');
+const { registerValidation, loginValidation } = require('../validation');
 
 
 
@@ -32,6 +32,19 @@ router.post('/register', async (req, res) => {
     } catch(err) {
         res.status(400).send(err);
     }
+});
+
+router.post('/login', async (req, res) => {
+    const { error } = loginValidation(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
+    const user = await User.findOne({username: req.body.username});
+    if(!user) return res.status(400).send('Username does not exists');
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if(!validPassword) return res.status(400).send('Invalid password');
+
+    res.send('Logged in');
 });
 
 module.exports = router;
