33852261235a2ab3fdc7fb3336218bf975d29795
Astrid <astrid.sinabian@chasacademy.se>
2019-09-28 20:51:23 +0200

    Search Component, search function works (backend)

---
+++ client/src/components/SearchUser.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SearchUser extends Component {
+    render() { 
+        return ( 
+            <div style={{display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+                <h3>Sök efter användare</h3>
+            </div>
+         );
+    }
+}
+ 
+export default SearchUser;
