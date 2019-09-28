22da82a854a077cd4ae1edfeaacf62ded0b25cef
Astrid <astrid.sinabian@chasacademy.se>
2019-09-28 20:10:54 +0200

    Navbar fixed (login/register/logout links depending on if logged in

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,23 +1,50 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import AuthService from './AuthService';
+import { withRouter } from 'react-router-dom';
 
 class NavBar extends Component {
     
     constructor(props) {
         super(props);
+
+        this.handleLogout = this.handleLogout.bind(this);
         this.Auth = new AuthService();
     }
 
+    handleLogout(e) {
+        e.preventDefault();
+        this.Auth.logout();
+        this.props.history.replace('/login');
+    }
+
+    componentDidMount() {
+
+    }
+
     render() { 
+        let links;
+
+        if(this.Auth.getToken() !== null) {
+            links = ( 
+                <button onClick={this.handleLogout}>Logga ut</button>
+            )
+        } else {
+            links = (
+                <div>
+                    <Link to="/login">Logga in</Link>
+                    <Link to="/register">Registrera dig</Link>
+                </div>
+            )
+        }
+
         return ( 
             <div>
                 <Link to="/">Logo</Link>
-                <Link to="/register">Registrera dig</Link>
-                <Link to="/login">Logga in</Link>
+                { links }
             </div>
         );
     }
 }
  
-export default NavBar;
+export default withRouter(NavBar);
