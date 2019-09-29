e763588abb2dfbd712ca75512af767f05a737c3c
Astrid Sinabian <42962371+astridsinabian@users.noreply.github.com>
2019-09-29 12:44:55 +0200

    Revert "Search"

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,6 @@ import Login from './components/Login';
 import FrontPage from './components/FrontPage';
 import PrivateRoute from './components/PrivateRoute';
 import OpenProfile from './components/OpenProfile';
-import SearchUser from './components/SearchUser';
 
 const Container = styled.div`
   margin: 0;
@@ -27,7 +26,6 @@ class App extends Component {
             <PrivateRoute path="/profile" component={Profile} />
             <Route path="/register" component={Register} />
             <Route path="/login" component={Login} />
-            <Route path="/user" exact component={SearchUser} />
             <Route path="/user/:username" exact component={OpenProfile} />
             <Route path="*" component={() => "404 NOT FOUND"} />
           </Switch>
