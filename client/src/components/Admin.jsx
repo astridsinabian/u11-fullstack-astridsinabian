b2751e287cebd181c59c58af184db06cde46295e
Astrid <astrid.sinabian@chasacademy.se>
2019-10-04 16:17:46 +0200

    Get users from back to front works

--- client/src/components/Admin.jsx
+++ client/src/components/Admin.jsx
@@ -1,15 +1,42 @@
 import React, { Component } from 'react';
 import AuthService from './AuthService';
 import { Redirect } from 'react-router-dom';
+import axios from 'axios';
 
 class Admin extends Component {
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            users: []
+        };
+
         this.Auth = new AuthService();
     }
 
+    getUsers = () => {
+        axios.get('http://localhost:5000/api/user/admin/users')
+            .then(res => this.setState({
+                users: res.data
+            }))
+            .catch(err => console.log(err));
+    }
+
+    componentDidMount() {
+        this.getUsers();
+    }
+
     render() { 
+
+        const users = this.state.users.map((user, key) => {
+            return <li key={user._id}>{user.firstname} {user.lastname}
+                        <ul>
+                            <li>{user.username}</li>
+                            <li>{user.email}</li>
+                        </ul>
+                    </li>
+        });
 
         if(this.Auth.isAdmin() === 'false') {
             return (
@@ -19,6 +46,7 @@ class Admin extends Component {
             return ( 
                 <div>
                     <h2>Admin - Översikt</h2>
+                    <ul>{ users }</ul>
                 </div>
             );
         }
