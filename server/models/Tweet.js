1312cf97b187139cd8f20ba24a1717269d5aa22f
Astrid <astrid.sinabian@chasacademy.se>
2020-01-02 19:15:59 +0100

    Formatted backend files

--- server/models/Tweet.js
+++ server/models/Tweet.js
@@ -1,22 +1,22 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const tweetSchema = new mongoose.Schema({
-    publisher: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    }, 
-    username: {
-        type: String,
-        trim: true
-    },
-    text: {
-        type: String,
-        max: 400
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
+  publisher: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  },
+  username: {
+    type: String,
+    trim: true
+  },
+  text: {
+    type: String,
+    max: 400
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
-module.exports = mongoose.model('Tweet', tweetSchema);
+module.exports = mongoose.model("Tweet", tweetSchema);
--- server/models/User.js
+++ server/models/User.js
@@ -1,54 +1,54 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unqiue: true,
-        trim: true,
-        minlength: 6
-    }, 
-    firstname: {
-        type: String,
-        required: true,
-        unqiue: true,
-        trim: true
-    },
-    lastname: {
-        type: String,
-        required: true,
-        unqiue: true,
-        trim: true
-    },
-    password: {
-        type: String,
-        required: true,
-        unqiue: true,
-        trim: true,
-        minlength: 6
-    },
-    email: {
-        type: String,
-        required: true,
-        unqiue: true,
-        trim: true,
-        max: 255
-    },
-    description: {
-        type: String,
-        required: false,
-        trim: true,
-        max: 400
-    },
-    followers: [],
-    following: [],
-    admin: {
-        type: Boolean
-    },
-    date: {
-        type: Date,
-        default: Date.now
-    }
+  username: {
+    type: String,
+    required: true,
+    unqiue: true,
+    trim: true,
+    minlength: 6
+  },
+  firstname: {
+    type: String,
+    required: true,
+    unqiue: true,
+    trim: true
+  },
+  lastname: {
+    type: String,
+    required: true,
+    unqiue: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true,
+    unqiue: true,
+    trim: true,
+    minlength: 6
+  },
+  email: {
+    type: String,
+    required: true,
+    unqiue: true,
+    trim: true,
+    max: 255
+  },
+  description: {
+    type: String,
+    required: false,
+    trim: true,
+    max: 400
+  },
+  followers: [],
+  following: [],
+  admin: {
+    type: Boolean
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
