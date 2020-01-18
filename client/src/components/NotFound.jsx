e4fc05ec71a7e20a61c6bec7c16a06533da9cf79
Astrid <astrid.sinabian@chasacademy.se>
2020-01-18 11:48:04 +0100

    Added responsive styling for Not Found page

--- client/src/components/NotFound.jsx
+++ client/src/components/NotFound.jsx
@@ -4,18 +4,29 @@ import styled from "styled-components";
 import NotFoundImg from "../assets/404-not-found.png";
 
 const NotFoundWrapper = styled.div`
-    display: flex;
-    justify-content: center;
-    margin-top: 6em;
+  display: flex;
+  justify-content: center;
+  margin-top: 6em;
+
+  @media (min-width: 320px) and (max-width: 480px) {
+    margin-top: 7em;
+  }
+`;
+
+const StyledImg = styled.img`
+  @media (min-width: 320px) and (max-width: 480px) {
+    width: 70%;
+    height: 70%;
+  }
 `;
 
 class NotFound extends Component {
   render() {
     return (
-        <NotFoundWrapper>
-            <Navbar />
-            <img src={NotFoundImg} alt="Not Found"/>
-        </NotFoundWrapper>
+      <NotFoundWrapper>
+        <Navbar />
+        <StyledImg src={NotFoundImg} alt="Not Found" />
+      </NotFoundWrapper>
     );
   }
 }
