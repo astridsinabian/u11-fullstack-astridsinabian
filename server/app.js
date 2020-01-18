a6e1027d64da83474f60cb682d86f39d0c9d1a33
astridsinabian <astrid.sinabian@chasacademy.se>
2020-01-18 14:33:27 +0100

    adding message

--- server/app.js
+++ server/app.js
@@ -1,14 +1,15 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 const app = express();
 const port = process.env.PORT || 5000;
+const path = require("path");
 
-const authRoute = require('./routes/auth');
-const tweets = require('./routes/tweets');
-const adminRoute = require('./routes/admin');
+const authRoute = require("./routes/auth");
+const tweets = require("./routes/tweets");
+const adminRoute = require("./routes/admin");
 
 // Configures enviroment varibles in dotenv-file
 dotenv.config();
@@ -16,20 +17,29 @@ dotenv.config();
 app.use(cors());
 
 // Connect to DB
-mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useCreateIndex: true }, () => 
-    console.log("MongoDB connection established succesfully!")
+mongoose.connect(
+  process.env.MONGODB_URI,
+  { useNewUrlParser: true, useCreateIndex: true },
+  () => console.log("MongoDB connection established succesfully!")
 );
 
 // Middlewares
 app.use(express.json());
 
 // Route middlewares
-app.use('/api/user', authRoute);
-app.use('/api/tweets', tweets);
-app.use('/api/admin', adminRoute);
+app.use("/api/user", authRoute);
+app.use("/api/tweets", tweets);
+app.use("/api/admin", adminRoute);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("../client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../", "client", "build", "index.html"));
+  });
+}
 
 // What starts the server
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+  console.log(`Server is running on port: ${port}`);
 });
-
