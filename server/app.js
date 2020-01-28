1275c4b49a5e397586b03b33fc7e4c24fa1ae383
Astrid <astrid.sinabian@chasacademy.se>
2020-01-28 12:44:01 +0100

    Fix

--- server/app.js
+++ server/app.js
@@ -31,12 +31,12 @@ app.use("/api/user", authRoute);
 app.use("/api/tweets", tweets);
 app.use("/api/admin", adminRoute);
 
-app.use(express.static(path.join(__dirname, "../client/build")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../", "client", "build", "index.html"));
-});
-
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static("../client/build"));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../", "client", "build", "index.html"));
+  });
+}
 
 // What starts the server
 app.listen(port, () => {
